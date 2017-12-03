f3b50e7d0c06da8a8d5c72b3df039eb91ef9445b
Catalin <catalin94v@gmail.com>
2017-12-03 18:51:22 +0000

    Get Coin Info

--- app/src/main/java/catalin/coinnews/MainActivity.java
+++ app/src/main/java/catalin/coinnews/MainActivity.java
@@ -1,15 +1,28 @@
 package catalin.coinnews;
 
+import android.accounts.NetworkErrorException;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import catalin.coinnews.models.Coin;
+import catalin.coinnews.services.CoinService;
+import catalin.coinnews.services.CoinServiceImpl;
+
 public class MainActivity extends AppCompatActivity {
 
     private static Context contextOfApplication;
     private Button getCoin;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         contextOfApplication = getApplicationContext();
 
+        getCoin = findViewById(R.id.getCoins);
+
         getCoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                new getData().execute();
             }
         });
 
@@ -29,5 +44,40 @@ public class MainActivity extends AppCompatActivity {
 
     public static Context getContextOfApplication(){
         return contextOfApplication;
+    }
+
+    class getData extends AsyncTask<String, Void, ArrayList<Coin>>  {
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+            if (progressDialog == null) {
+                progressDialog = ProgressDialog.show(MainActivity.this, "Please wait.", "Retrieving data...",true );
+            }
+        }
+
+        @Override
+        protected ArrayList<Coin> doInBackground(String... strings) {
+            CoinService cs;
+            Coin coin = null;
+            ArrayList<Coin> coins = new ArrayList<>();
+
+            cs = new CoinServiceImpl();
+
+            try {
+                coins = cs.getCoins();
+            } catch (IOException | NetworkErrorException |JSONException e) {
+                e.printStackTrace();
+            }
+            return coins;
+        }
+
+        protected void onPostExecute(ArrayList<Coin> coins) {
+            if (progressDialog != null) {
+                try {
+                    progressDialog.dismiss();
+                } catch (Exception ex) {}
+                progressDialog = null;
+            }
+        }
     }
 }
--- app/src/main/java/catalin/coinnews/services/CoinServiceImpl.java
+++ app/src/main/java/catalin/coinnews/services/CoinServiceImpl.java
@@ -29,21 +29,21 @@ public class CoinServiceImpl extends Service implements CoinService {
 
         if (result) {
             final String jsonData = response.body().string();
+            JSONArray jsonArray = new JSONArray(jsonData);
 
-//            JSONObject jsonResponse = new JSONObject(jsonData);
-//
-//            JSONArray jsonTransfers = jsonResponse.getJSONArray("data");
-//            JSONObject jsonTransfer;
-//            Coin coin;
-//            for (int i=0; i<jsonTransfers.length(); i++) {
-//                jsonTransfer = jsonTransfers.getJSONObject(i);
-//                stock = new Stock();
-//                stock.setTitle(title);
-//                stock.setStockCount(stockCount);
-//
-//                stocks.add(stock);
-//            }
-//        }
+            Coin coin;
+            for (int i=0; i<jsonArray.length(); i++) {
+                coin = new Coin();
+                JSONObject jsonObj = new JSONObject(jsonArray.get(i).toString());
+                coin.setId(jsonObj.getString("id"));
+                coin.setName(jsonObj.getString("name"));
+                coin.setSymbol(jsonObj.getString("symbol"));
+                coin.setRank(Integer.parseInt(jsonObj.getString("rank")));
+                coin.setPrice_usd(Float.parseFloat(jsonObj.getString("price_usd")));
+                coin.setPrice_btc(Float.parseFloat(jsonObj.getString("price_btc")));
+
+                coins.add(coin);
+            }
         }
         return coins;
     }
